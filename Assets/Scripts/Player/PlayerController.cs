d2e45a8bccad7921d8fd4c56c79a9d5493c394af
Sangmin1008 <dkdhakzm@gmail.com>
2025-05-12 14:40:14 +0900

    충돌 수정

--- Assets/Scripts/Player/PlayerController.cs
+++ Assets/Scripts/Player/PlayerController.cs
@@ -6,7 +6,6 @@ public class PlayerController : MonoBehaviour
     private InputHandler inputHandler;
     private PlayerVisual playerVisual;
     private bool isInitialized = false;
-    public bool isMoveing = false; //WeaponAttack
 
     public void Init(PlayerVisual visual)
     {
@@ -19,21 +18,18 @@ public class PlayerController : MonoBehaviour
     {
         if (!isInitialized)
         {
-            throw new System.Exception("PlayerController�� �ʱ�ȭ���� �ʾҽ��ϴ�.");
+            throw new System.Exception("PlayerController가 초기화되지 않았습니다.");
         }
         moveDirection = inputHandler.MoveInput;
         if (moveDirection == Vector2.zero)
         {
             playerVisual.SetMoving(false);
-            isMoveing = false;
             return;
         }
-       
 
-        //Todo - ���� ���ȿ��� �ӵ� �޾ƿ�����
+        //Todo - 추후 스탯에서 속도 받아오도록
         transform.Translate(moveDirection * 3f * Time.deltaTime);
         playerVisual.SetDirection(moveDirection);
         playerVisual.SetMoving(true);
-        isMoveing = true;
     }
 }
