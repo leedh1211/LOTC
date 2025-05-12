599b7d6f35e2c3944c38900924342797a7946077
Sangmin1008 <dkdhakzm@gmail.com>
2025-05-12 14:16:41 +0900

    feat : MainGameController

--- Assets/Sangmin1008/Scripts/HealthBarUI.cs
+++ Assets/Sangmin1008/Scripts/HealthBarUI.cs
@@ -1,18 +1,89 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class HealthBarUI : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    [SerializeField] private Slider slider;
+    [SerializeField] private IntegerEventChannelSO playerHitEvent;
+    [SerializeField] private IntegerEventChannelSO playerHealEvent;
+    [SerializeField] private VoidEventChannelSO playerDeathEvent;
+
+    [SerializeField] private int maxHp = 100;
+    private int _currentHp;
+    private Coroutine _sliderCoroutine;
+
+    private void OnEnable()
     {
-        
+        playerHitEvent.OnEventRaised += TakeDamage;
+        playerHealEvent.OnEventRaised += Heal;
     }
 
-    // Update is called once per frame
-    void Update()
+    private void OnDisable()
     {
-        
+        playerHitEvent.OnEventRaised -= TakeDamage;
+        playerHealEvent.OnEventRaised -= Heal;
+    }
+
+    private void Start()
+    {
+        _currentHp = maxHp;
+        slider.minValue = 0;
+        slider.maxValue = maxHp;
+        slider.value = _currentHp;
+    }
+
+    private void TakeDamage(int damage)
+    {
+        int newHp = Mathf.Max(_currentHp - damage, 0);
+
+        if (_sliderCoroutine != null)
+            StopCoroutine(_sliderCoroutine);
+
+        _sliderCoroutine = StartCoroutine(SmoothSliderUpdate(_currentHp, newHp));
+
+        if (newHp <= 0)
+        {
+            playerDeathEvent.Raise();
+        }
+    }
+
+    private void Heal(int amount)
+    {
+        int newHp = Mathf.Min(_currentHp + amount, maxHp);
+
+        if (_sliderCoroutine != null)
+            StopCoroutine(_sliderCoroutine);
+
+        _sliderCoroutine = StartCoroutine(SmoothSliderUpdate(_currentHp, newHp));
+    }
+
+    private IEnumerator SmoothSliderUpdate(int from, int to)
+    {
+        float duration = 0.4f;
+        float elapsed = 0f;
+
+        float startValue = from;
+        float targetValue = to;
+
+        while (elapsed < duration)
+        {
+            elapsed += Time.deltaTime;
+            float newValue = Mathf.Lerp(startValue, targetValue, elapsed / duration);
+            slider.value = newValue;
+            yield return null;
+        }
+
+        slider.value = targetValue;
+        _currentHp = to;
+        _sliderCoroutine = null;
+    }
+    
+    [ContextMenu("PlayerHit!!")]
+    private void PlayerHit()
+    {
+        playerHitEvent.Raise(20);
     }
 }
